629616bfacb0ef4f363412b8b5f8043ead042348
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-07-06 16:44:52 +0200

    [ADD] Add base of system manager

---
+++ include/kronkworld/system/System.hpp
@@ -0,0 +1,30 @@
+/*
+** FREE PROJECT, 2026
+** KRONKWORLD
+** File description:
+** World (mediator ?)
+*/
+#ifndef _KRONKWORLD_SYSTEM_H
+    #define _KRONKWORLD_SYSTEM_H
+    #include "../entity/Entity.hpp"
+    #include "../component/Component.hpp"
+    #include <iostream>
+    #include <utility>
+#include <vector>
+
+namespace kw
+{
+
+    class SystemManager
+    {
+    public:
+        
+
+    private:
+        // std::vector<ILogicSystem> m_logicSystems;
+        // std::vector<IRenderSystem> m_renderSystems;
+    };
+
+}
+
+#endif /* _KRONKWORLD_SYSTEM_H */
